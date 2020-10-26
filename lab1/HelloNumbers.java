ec21ed100de111971cfe12ce21a958ba968c3af1
xdxdz <xdxdzhang@gmail.com>
2020-10-25 19:53:18 -0700

    complted first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+    public static void main(String[] args){
+      int x = 0;
+      int sum = 0;
+      while (x<10){
+        System.out.println(sum);
+        x = x + 1;
+        sum = sum + x;
+      }
+      /* x = "horse";*/
+
+    }
+}
